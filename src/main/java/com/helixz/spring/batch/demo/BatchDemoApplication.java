68f842067fc1b2cc5f0967e700f10951ffcf0654
Chamith Kodikara <chamith@ceylonit.com>
2019-08-19 13:00:34 +0530

    -initial commit

---
+++ src/main/java/com/helixz/spring/batch/demo/BatchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.helixz.spring.batch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchDemoApplication.class, args);
+    }
+
+}
