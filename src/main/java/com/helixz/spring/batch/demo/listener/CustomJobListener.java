88c4cd13e615d6902070626cc2e87b67a8dbca8a
Chamith Kodikara <chamith@ceylonit.com>
2019-08-30 14:12:47 +0530

    -code cleanup

--- src/main/java/com/helixz/spring/batch/demo/listener/CustomJobListener.java
+++ src/main/java/com/helixz/spring/batch/demo/listener/CustomJobListener.java
@@ -5,7 +5,7 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.stereotype.Component;
 
-import static org.springframework.batch.core.BatchStatus.COMPLETED;
+import static org.springframework.batch.core.BatchStatus.*;
 
 /**
  * @author Chamith
@@ -14,6 +14,12 @@ import static org.springframework.batch.core.BatchStatus.COMPLETED;
 @Component
 public class CustomJobListener extends JobExecutionListenerSupport {
 
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        if (jobExecution.getStatus() == STARTED) {
+            log.info("ORDER BATCH PROCESS STARTED...!");
+        }
+    }
     @Override
     public void afterJob(JobExecution jobExecution) {
         if (jobExecution.getStatus() == COMPLETED) {
